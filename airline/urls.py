163b523543477c2022bfcd90a2eccf944c84808f
bobm1993 <bob.m.1993@gmail.com>
2016-09-21 20:54:18 +0300

    dispatcher fixed

--- airline/urls.py
+++ airline/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from airline_app.admin_flights_list import add_flight, delete_flight, get_flights
 from airline_app.admin_users_list_view import users_list, delete_user
-from airline_app.dispatcher_views import get_dispatcher, view_team
+from airline_app.dispatcher_views import get_dispatcher, view_team, create_team, save_team
 from airline_app.views import RegistrationView, LoginView, GetUsersView, GetFlightsView
 
 urlpatterns = [
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^delete_flight/([\w]+)$', delete_flight),
     url(r'^get_dispatcher$', get_dispatcher),
     url(r'^view_team/([\w]+)$', view_team),
-    url(r'^create_team/([\w]+)$', create_team),
+    url(r'^create_team$', create_team),
+    url(r'^save_team/([\w]+)$', save_team)
 ]
--- airline_app/dispatcher_views.py
+++ airline_app/dispatcher_views.py
@@ -1,8 +1,10 @@
 import json
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view
-from airline_app.models import Flights
+from rest_framework.parsers import JSONParser
+from airline_app.models import Flights, Team, Users
 
 
 @api_view(['GET'])
@@ -29,14 +31,62 @@ def view_team(request, f_id):
         flight = Flights.objects.filter(id=f_id).first()
         team = flight.id_team
         json_team = {'team': []}
-        temp = {'pilot1': team.id_pilot1__name + ' ' + team.id_pilot1__surname,
-                'pilot2': team.id_pilot2__name + ' ' + team.id_pilot3__surname,
-                'dispatcher': team.id_dispatcher__name + ' ' + team.id_dispatcher__surname,
-                'stewardess1': team.id_stewardess1__name + ' ' + team.id_stewardess1__surname,
-                'stewardess2': team.id_stewardess2__name + ' ' + team.id_stewardess2__surname,
-                'radioman': team.id_radioman__name + ' ' + team.id_radioman__surname}
+        temp = {'pilot1': team.id_pilot1.name + ' ' + team.id_pilot1.surname,
+                'pilot2': team.id_pilot2.name + ' ' + team.id_pilot2.surname,
+                'navigator': team.id_navigator.name + ' ' + team.id_navigator.surname,
+                'stewardess1': team.id_stewardess1.name + ' ' + team.id_stewardess1.surname,
+                'stewardess2': team.id_stewardess2.name + ' ' + team.id_stewardess2.surname,
+                'radioman': team.id_radioman.name + ' ' + team.id_radioman.surname}
         json_team['team'].append(temp)
         return HttpResponse(json.dumps(json_team), content_type='application/json',
                             status=status.HTTP_200_OK)
+    else:
+        return HttpResponse('error login', status=status.HTTP_401_UNAUTHORIZED)
+
+
+@api_view(['GET'])
+def create_team(request):
+    if request.session.get('user_e_mail') is not None and request.session.get('user_role') == 2:
+        pilots = Users.objects.filter(id_role=3)
+        navigators = Users.objects.filter(id_role=4)
+        stewardess = Users.objects.filter(id_role=5)
+        radiomen = Users.objects.filter(id_role=6)
+        json_personal = {'pilots': [], 'navigators': [], 'radiomen': [], 'stewardess': []}
+        for pilot in pilots:
+            temp = {'id': pilot.id, 'name': pilot.name, 'surname': pilot.surname}
+            json_personal['pilots'].append(temp)
+        for navigator in navigators:
+            temp = {'id': navigator.id, 'name': navigator.name, 'surname': navigator.surname}
+            json_personal['navigators'].append(temp)
+        for radioman in radiomen:
+            temp = {'id': radioman.id, 'name': radioman.name, 'surname': radioman.surname}
+            json_personal['radiomen'].append(temp)
+        for st in stewardess:
+            temp = {'id': st.id, 'name': st.name, 'surname': st.surname}
+            json_personal['stewardess'].append(temp)
+        return HttpResponse(json.dumps(json_personal), content_type='application/json',
+                            status=status.HTTP_200_OK)
+    else:
+        return HttpResponse('error login', status=status.HTTP_401_UNAUTHORIZED)
+
+
+@csrf_exempt
+@api_view(['GET', 'POST'])
+def save_team(request, f_id):
+    if request.session.get('user_e_mail') is not None and request.session.get('user_role') == 2:
+        json_data = JSONParser().parse(request)
+        pilot1 = Users.objects.filter(id=json_data['pilot1']).first()
+        pilot2 = Users.objects.filter(id=json_data['pilot2']).first()
+        navigator = Users.objects.filter(id=json_data['navigator']).first()
+        radioman = Users.objects.filter(id=json_data['radioman']).first()
+        stewardess1 = Users.objects.filter(id=json_data['stewardess1']).first()
+        stewardess2 = Users.objects.filter(id=json_data['stewardess2']).first()
+        t = Team(id_pilot1=pilot1, id_pilot2=pilot2, id_navigator=navigator, id_radioman=radioman,
+                 id_stewardess1=stewardess1, id_stewardess2=stewardess2)
+        t.save()
+        flight = Flights.objects.filter(id=f_id).first()
+        flight.id_team = t
+        flight.save()
+        return HttpResponse('ok', status=status.HTTP_200_OK)
     else:
         return HttpResponse('error login', status=status.HTTP_401_UNAUTHORIZED)
